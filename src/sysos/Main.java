17f12c49b3cde5a4d184870250a422399393d475
Filip Pietraszak <33572448+Pietrucci@users.noreply.github.com>
2018-01-25 23:48:28 +0100

    PAGES - wyświetlanie tablicy stronic

--- src/sysos/Main.java
+++ src/sysos/Main.java
@@ -5,32 +5,31 @@ import java.util.Scanner;
 import java.io.File;
 
 public class Main {
-		
+
 	public static Memory M = new Memory();
 	public static process_manager T = new process_manager();
 	public static FileSystem F = new FileSystem();
 	public static int OBECNY_PROCES;
 	public static schedulerr S = new schedulerr();
 	public static plik1 P = new plik1();
-	
+
 	public static void main(String[] args) {
 		systemLoadLite();
 		T.init();
-		S.runningProcess=T.INIT;
+		S.runningProcess = T.INIT;
 		String string;
 		Scanner in = new Scanner(System.in);
 		do {
 			System.out.print("user@sos:~$ ");
-			
+
 			string = in.nextLine();
 			String[] tab = string.split(" ");
-			if(tab.length > 0) {
+			if (tab.length > 0) {
 				tab[0] = tab[0].toUpperCase();
 			}
-			
-			if(tab[0].equals("HELP"))
-			{
-				if(tab.length==1) {
+
+			if (tab[0].equals("HELP")) {
+				if (tab.length == 1) {
 					System.out.println("OF [nazwa_pliku]         : otwieranie wskazanego pliku");
 					System.out.println("CLF [nazwa_pliku]        : zamykanie wskazanego pliku");
 					System.out.println("DF [nazwa_pliku]         : usuwanie wskazanego pliku");
@@ -42,92 +41,82 @@ public class Main {
 					System.out.println("PB [nr_bloku]            : sprawdzenie pojedynczego bloku dysku");
 					System.out.println("PING [nazwa_pliku]       : ping adresu ip wskazanego w pliku");
 					System.out.println("MEMORY                   : sprawdzanie stanu pamieci");
+					System.out.println("PAGES [id_procesu]       : sprawdzanie tablicy stronic procesu");
 					System.out.println("TASKLIST                 : sprawdzanie listy procesow");
 					System.out.println("TASKKILL [nazwa_procesu] : zabijanie procesu; ");
 					System.out.println("GO                       : kolejny krok wykonywanego procesu");
 					System.out.println("GOM [n]                  : n kolejnych krokow wykonywanego procesu");
 					System.out.println("START [nazwa_procesu rezerwowane_miejsce nazwa_pliku]\n"
-									 + "                         : stworzenie procesu");
+							+ "                         : stworzenie procesu");
 					System.out.println("STPRI [nazwa_procesu rezerwowane_miejsce nazwa_pliku priorytet]\n"
-							 + "                         : stworzenie procesu o podanym priorytecie");
+							+ "                         : stworzenie procesu o podanym priorytecie");
 					System.out.println("SREG                     : wyswietla rejestry");
 					System.out.println("exit                     : wyjscie ");
-					
-				}else
+
+				} else
 					System.out.println("nieprawidlowe wywolanie komendy");
 			}
-			
-			else if(tab[0].equals("OF"))
-			{
-				if(tab.length==2)
+
+			else if (tab[0].equals("OF")) {
+				if (tab.length == 2)
 					F.openFile(tab[1]);
 				else
 					System.out.println("nieprawidlowe wywolanie komendy");
 			}
-			
-			else if(tab[0].equals("CLF"))
-			{
-				if(tab.length==2)
+
+			else if (tab[0].equals("CLF")) {
+				if (tab.length == 2)
 					F.closeFile(tab[1]);
 				else
 					System.out.println("nieprawidlowe wywolanie komendy");
 			}
-			
-			else if(tab[0].equals("DF"))
-			{
-				if(tab.length==2)
+
+			else if (tab[0].equals("DF")) {
+				if (tab.length == 2)
 					F.deleteFile(tab[1]);
 				else
 					System.out.println("nieprawidlowe wywolanie komendy");
 			}
-			
-			else if(tab[0].equals("CF"))
-			{
-				if(tab.length==2)
+
+			else if (tab[0].equals("CF")) {
+				if (tab.length == 2)
 					F.createFile(tab[1]);
 				else
 					System.out.println("nieprawidlowe wywolanie komendy");
 			}
-			
-			
-			else if(tab[0].equals("WF"))
-			{
-				if(tab.length>2)
-				{
-//					String temp = "";
-//					for (int i = 2; i < tab.length; i++) {
-//						temp += tab[i];
-//						temp += " ";
-//					}
+
+			else if (tab[0].equals("WF")) {
+				if (tab.length > 2) {
+					// String temp = "";
+					// for (int i = 2; i < tab.length; i++) {
+					// temp += tab[i];
+					// temp += " ";
+					// }
 					String temp = string.substring(4 + tab[1].length());
 					System.out.println(temp);
 					F.writeFile(tab[1], temp);
-				}
-				else
+				} else
 					System.out.println("nieprawidlowe wywolanie komendy");
 			}
-			
-			else if(tab[0].equals("RF"))
-			{
-				if(tab.length==2)
+
+			else if (tab[0].equals("RF")) {
+				if (tab.length == 2)
 					F.readFile(tab[1]);
 				else
 					System.out.println("nieprawidlowe wywolanie komendy");
-			
+
 			}
-			
-			else if(tab[0].equals("PD"))
-			{
-				if(tab.length==1) {
+
+			else if (tab[0].equals("PD")) {
+				if (tab.length == 1) {
 					F.printDisc();
-					
-				}else
+
+				} else
 					System.out.println("nieprawidlowe wywolanie komendy");
 			}
-			
-			else if(tab[0].equals("PB"))
-			{
-				if(tab.length==2) {
+
+			else if (tab[0].equals("PB")) {
+				if (tab.length == 2) {
 					try {
 						int blockI = Integer.parseInt(tab[1]);
 						F.printSector(blockI);
@@ -135,87 +124,89 @@ public class Main {
 						System.out.println("nieprawidlowe wywolanie komendy");
 					}
 
-					
-				}else
+				} else
 					System.out.println("nieprawidlowe wywolanie komendy");
 			}
-			
-			else if(tab[0].equals("LS"))
-			{
-				if(tab.length==1) {
-					F.listAllFiles();					
-				}else
+
+			else if (tab[0].equals("LS")) {
+				if (tab.length == 1) {
+					F.listAllFiles();
+				} else
 					System.out.println("nieprawidlowe wywolanie komendy");
 			}
-			
-			else if(tab[0].equals("PING")) {
-				if(tab.length == 2) {
+
+			else if (tab[0].equals("PING")) {
+				if (tab.length == 2) {
 					F.executePingFromFile(tab[1]);
-				}
-				else {
+				} else {
 					System.out.println("nieprawidlowe wywolanie komendy");
 				}
 			}
-			
-			else if(tab[0].equals("MEMORY"))
-			{
-				if(tab.length==1) {
+
+			else if (tab[0].equals("MEMORY")) {
+				if (tab.length == 1) {
 					M.printMemory();
-					
-				}else
+				} else
 					System.out.println("nieprawidlowe wywolanie komendy");
 			}
-			
-			else if(tab[0].equals("TASKLIST"))
-			{
-				if(tab.length==1) {
-					T.INIT.show_list();
-					
-				}else
-					System.out.println("nieprawidlowe wywolanie komendy");
-			}
-			
-			else if(tab[0].equals("TASKKILL"))
-			{
-				if(tab.length==2)
-					//tab[1] to nazwa procesu
-					T.INIT.kill(T.find_name(tab[1]));
-				
-				else
-					System.out.println("nieprawidlowe wywolanie komendy");
-			}
-			
-			else if(tab[0].equals("GO"))
-			{
-				if(tab.length==1) {
-					interpreter i =new interpreter(M,F);
-					i.exe();
-					//stan procesu po wykonaniu jednego kroku 
-				}
-				else
-					System.out.println("nieprawidlowe wywolanie komendy");
-			}
-			
-			else if(tab[0].equals("GOM")) {
-				if(tab.length == 2) {
-					for(int j=0;j < Integer.parseInt(tab[1]); j++) {
-						interpreter i =new interpreter(M,F);
-						i.exe();
-						//stan procesu po wykonaniu jednego kroku 
+
+			else if (tab[0].equals("PAGES")) {
+				if (tab.length == 2) {
+					int id = 0;
+					try {
+						id = Integer.parseInt(tab[1]);
+						if (T.find(id) != null)
+							T.find(id).printPages();
+					} catch (NumberFormatException nfe) {
+						System.out.println("nieprawidlowe wywolanie komendy");
 					}
-				}
-				else {
+
+				} else
+					System.out.println("nieprawidlowe wywolanie komendy");
+			}
+
+			else if (tab[0].equals("TASKLIST")) {
+				if (tab.length == 1) {
+					T.INIT.show_list();
+
+				} else
+					System.out.println("nieprawidlowe wywolanie komendy");
+			}
+
+			else if (tab[0].equals("TASKKILL")) {
+				if (tab.length == 2)
+					// tab[1] to nazwa procesu
+					T.INIT.kill(T.find_name(tab[1]));
+
+				else
+					System.out.println("nieprawidlowe wywolanie komendy");
+			}
+
+			else if (tab[0].equals("GO")) {
+				if (tab.length == 1) {
+					interpreter i = new interpreter(M, F);
+					i.exe();
+					// stan procesu po wykonaniu jednego kroku
+				} else
+					System.out.println("nieprawidlowe wywolanie komendy");
+			}
+
+			else if (tab[0].equals("GOM")) {
+				if (tab.length == 2) {
+					for (int j = 0; j < Integer.parseInt(tab[1]); j++) {
+						interpreter i = new interpreter(M, F);
+						i.exe();
+						// stan procesu po wykonaniu jednego kroku
+					}
+				} else {
 					System.out.println("nieprawidlowe wywolanie komendy");
 				}
 			}
-			
-			else if(tab[0].equals("START"))
-			{
-				if(tab.length==4) {
+
+			else if (tab[0].equals("START")) {
+				if (tab.length == 4) {
 					/*
-					 tab[1] nazwa procesu
-					 tab[2] rezerwoe miejsce
-					 tab[3] nazwa pliku
+					 * tab[1] nazwa procesu tab[2] rezerwoe miejsce tab[3] nazwa pliku
 					 */
 					String kod = new String();
 					Scanner scan;
@@ -225,25 +216,22 @@ public class Main {
 						int size = Integer.parseInt(tab[2]);
 						T.INIT.fork(tab[1]);
 						int x = T.find_name(tab[1]);
-						//S.check(T);
-						T.find(x).exec(kod, tab[3], kod.length()+size);
+						// S.check(T);
+						T.find(x).exec(kod, tab[3], kod.length() + size);
 						scan.close();
 					} catch (FileNotFoundException e) {
 						System.out.println("Nie znaleziono pliku");
 					}
-				
-				}else
+
+				} else
 					System.out.println("nieprawidlowe wywolanie komendy");
 			}
-			
-			else if(tab[0].equals("STPRI"))
-			{
-				if(tab.length==5) {
+
+			else if (tab[0].equals("STPRI")) {
+				if (tab.length == 5) {
 					/*
-					 tab[1] nazwa procesu
-					 tab[2] rezerwoe miejsce
-					 tab[3] nazwa pliku
-					 tab[4] priorytet
+					 * tab[1] nazwa procesu tab[2] rezerwoe miejsce tab[3] nazwa pliku tab[4]
+					 * priorytet
 					 */
 					String kod = new String();
 					Scanner scan;
@@ -253,37 +241,37 @@ public class Main {
 						int size = Integer.parseInt(tab[2]);
 						T.INIT.forkWithPri(tab[1], Integer.parseInt(tab[4]));
 						int x = T.find_name(tab[1]);
-						//S.check(T);
-						T.find(x).exec(kod, tab[3], kod.length()+size);
+						// S.check(T);
+						T.find(x).exec(kod, tab[3], kod.length() + size);
 						scan.close();
 					} catch (FileNotFoundException e) {
 						System.out.println("Nie znaleziono pliku");
 					}
-				
-				}else
+
+				} else
 					System.out.println("nieprawidlowe wywolanie komendy");
 			}
-			
-			else if(tab[0].equals("SREG")) {
-				if(tab.length == 1) {
-					System.out.println("Rejestry: A|" + S.runningProcess.A + " B|" + S.runningProcess.B + " C|" + S.runningProcess.C + " D|" + S.runningProcess.D);
-				}
-				else {
+
+			else if (tab[0].equals("SREG")) {
+				if (tab.length == 1) {
+					System.out.println("Rejestry: A|" + S.runningProcess.A + " B|" + S.runningProcess.B + " C|"
+							+ S.runningProcess.C + " D|" + S.runningProcess.D);
+				} else {
 					System.out.println("nieprawidlowe wywolanie komendy");
 				}
 			}
-			
-			else if(!string.equals("exit")){
+
+			else if (!string.equals("exit")) {
 				System.out.println("nie ma takiej komendy");
 			}
-			
-		}while(!string.equals("exit"));	
-			in.close();	
+
+		} while (!string.equals("exit"));
+		in.close();
 	}
-	
+
 	private static void systemLoadLite() {
-		
-		//java.awt.Toolkit.getDefaultToolkit().beep();
+
+		// java.awt.Toolkit.getDefaultToolkit().beep();
 		System.out.println("                           ********             ********");
 		System.out.println("                          **//////             **////// ");
 		System.out.println("                         /**          ******  /**       ");
@@ -293,7 +281,7 @@ public class Main {
 		System.out.println("                          ********  //******   ******** ");
 		System.out.println("                         ////////   //////   ////////  ");
 		System.out.println();
-		
+
 	}
 
 }
